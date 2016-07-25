9a3cae9f595ff6e725df8ed3789319e77c8d2c95
Himanshu Gupta <himanshu.gupta@tudip.nl>
2016-07-25 17:58:46 +0530

    Added routes for email subscription

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   post "/contact/thank-you".dasherize, to: "pages#contact_thank_you", as: "post_contact_thank_you_page"
   post "/owners/thank-you".dasherize, to: "pages#owners_thank_you", as: "post_owners_thank_you_page"
+  post "/subscribe/newsletter".dasherize, to: "pages#newsletter", as: "post_newsletter"
+  post "/faq/review".dasherize, to: "pages#faqreview", as: "post_faq_review"
   get '/offer', to: "home#offer"
 
   namespace :api do
